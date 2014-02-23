fae3c43924e1110f995f3f4cf11614183181277f
shengang1978 <shengang1978@hotmail.com>
2014-02-23 10:59:21 +0800

    For the Query1s test -- add the proj file.

--- LRCS_TST/Query1S.cpp
+++ LRCS_TST/Query1S.cpp
@@ -1,5 +1,6 @@
 #include "Query1S.h"
 #include "ValPos.h"
+#include "ProjMaker.h"
 
 Query1S::Query1S() {
 }
@@ -25,60 +26,126 @@ bool Query1S::run(char* arg1, char* arg2) {
 
 	string path = "../data/StringTestData.mid";
 
+	char* cszFile = "StringTest01";
+	char* cpProjExt = "proj";
+
+	std::string strProj = cszFile;
+	strProj += cpProjExt;
+
 	if (createcolumn){
 		Log::writeToLog("Query1S", 10, "Creating Column1...");
 		stopWatch.start();
-		SColumnExtracter* ce = new SColumnExtracter(path, 1, 10, forceBuild);
+
+		auto_ptr<ProjMaker> spProjMaker(new ProjMaker());	
+
+		auto_ptr<Projection> spProj(new Projection(strProj.c_str()));
+
+		PROJECTION_REC	projRec;
+		vector<COLUMN_REC> columns;
+		COLUMN_REC colRecID;
+		COLUMN_REC colRecDESC;
+
+		strncpy(projRec.projectionName, strProj.c_str(), MAX_NAME_LENGTH);
+		strncpy(projRec.primaryColumn, "ID", MAX_NAME_LENGTH);
+		strncpy(projRec.belongTable, "$sys$", MAX_NAME_LENGTH);
+
+		std::string strColName = cszFile;
+		strColName += ".ID";
+		strncpy(colRecID.projectionName, strProj.c_str(), MAX_NAME_LENGTH);
+		strncpy(colRecID.columnName, strColName.c_str(), MAX_NAME_LENGTH);
+		colRecID.columnPosition = 1;
+		colRecID.dataType = ValPos::STRINGTYPE;
+		colRecID.sorted = true;
+		colRecID.contiguous = true;
+		colRecID.compressionType = NO_COMPRESSION;
+		colRecID.dataLength = 10;
+		columns.push_back(colRecID);
+
+		SColumnExtracter* ce = new SColumnExtracter(path, colRecID.columnPosition, colRecID.dataLength, forceBuild);
 		StringEncoder* encoder = new StringEncoder((Operator*)ce, // data source
 			0,              // column index
 			NULL,
-			10,             // string Size
+			colRecID.dataLength,             // string Size
 			8 * PAGE_SIZE);   // buffer size in bits
-		StringDecoder* decoder = new StringDecoder(true);         // value sorted
+		StringDecoder* decoder = new StringDecoder(colRecID.sorted);         // value sorted
 		PagePlacer* pagePlacer = new PagePlacer(encoder,
 			decoder,
 			2,
 			true);
-		pagePlacer->placeColumn("StringTest01.ID", false, true,0);
+		pagePlacer->placeColumn(strColName, false, colRecID.sorted, 0);
+
+
 		delete pagePlacer;
 		delete ce;
 		delete encoder;
 		delete decoder;
 
 		Log::writeToLog("Query1S", 10, "Creating Column2...");
+
+		strColName = cszFile;
+		strColName += ".DESC";
+
+		strncpy(colRecDESC.projectionName, strProj.c_str(), MAX_NAME_LENGTH);
+		strncpy(colRecDESC.columnName, strColName.c_str(), MAX_NAME_LENGTH);
+		colRecDESC.columnPosition = 2;
+		colRecDESC.dataType = ValPos::STRINGTYPE;
+		colRecDESC.sorted = false;
+		colRecDESC.contiguous = false;
+		colRecDESC.compressionType = NO_COMPRESSION;
+		colRecDESC.dataLength = 11;
+		columns.push_back(colRecDESC);
+
 		//string path=RLE_EXTRACT_LOAD_AND_ACCESS_DATA_PATH;
-		ce = new SColumnExtracter(path, 2, 11, forceBuild);
+		ce = new SColumnExtracter(path, colRecDESC.columnPosition, colRecDESC.dataLength, forceBuild);
 		encoder = new StringEncoder((Operator*)ce, // data source
 			0,              // column index
 			NULL,
-			11,             // string Size
+			colRecDESC.dataLength,             // string Size
 			8 * PAGE_SIZE);   // buffer size in bits
-		decoder = new StringDecoder(false);         // value sorted
+		decoder = new StringDecoder(colRecDESC.sorted);         // value sorted
 		pagePlacer = new PagePlacer(encoder,
 			decoder,
 			2,
 			true);
-		pagePlacer->placeColumn("StringTest01.DESC", false, false, 0);
+		pagePlacer->placeColumn(strColName, false, colRecDESC.sorted, 0);
 		delete pagePlacer;
 		delete ce;
 		delete encoder;
 		delete decoder;
 		stopWatch.stop();
 		//system("move StringTest01* " RUNTIME_DATA);
+
+		spProj->setProjectionRec(&projRec);
+		spProj->setProjColumns(&columns);
+		spProjMaker->createProjection(spProj.get());
 	}
 
 	Log::writeToLog("Query1S", 10, "Opening Column1...");
 	stopWatch.start();
-	ROSAM* am1 = new ROSAM("StringTest01.ID", 2, sizeof(int), 10, ValPos::INTTYPE, ValPos::STRINGTYPE);
+
+	auto_ptr<ProjMaker> spProjMaker(new ProjMaker());
+
+	char szProj[MAX_NAME_LENGTH];
+	
+	strncpy(szProj, strProj.c_str(), MAX_NAME_LENGTH);
+	Projection* proj = spProjMaker->getProjection(szProj);
+
+
+	PROJECTION_REC* projRecPtr = proj->getProjectionRec();
+	vector<COLUMN_REC>* columnsPtr = proj->getProjColumns();
+
+	//delete spProjMaker;
+	
+	ROSAM* am1 = new ROSAM((*columnsPtr)[0].columnName, 2, sizeof(int), (*columnsPtr)[0].dataLength, ValPos::INTTYPE, (*columnsPtr)[0].dataType);
 	Log::writeToLog("Query1S", 10, "Opening Column2...");
-	ROSAM* am2 = new ROSAM("StringTest01.DESC", 2, sizeof(int), 11, ValPos::INTTYPE, ValPos::STRINGTYPE);
+	ROSAM* am2 = new ROSAM((*columnsPtr)[1].columnName, 2, sizeof(int), (*columnsPtr)[1].dataLength, ValPos::INTTYPE, (*columnsPtr)[1].dataType);
 	Predicate* pred1 = new Predicate(Predicate::OP_GREATER_THAN_OR_EQUAL);
 	Predicate* pred2 = new Predicate(Predicate::OP_LESS_THAN);
-	StringDataSource* ds1 = new StringDataSource(am1, true, true);
-	StringDataSource* ds2 = new StringDataSource(am2, false, true);
+	StringDataSource* ds1 = new StringDataSource(am1, (*columnsPtr)[0].sorted, true);
+	StringDataSource* ds2 = new StringDataSource(am2, (*columnsPtr)[1].sorted, true);
 
 	ds1->setPredicate(pred1);
-	//ds2->setPredicate(pred2);
+	ds2->setPredicate(pred2);
 
 	ValPos* rhs1 = new StringValPos(10);
 	char* RHSVal1 = "abcde99990";
@@ -86,9 +153,9 @@ bool Query1S::run(char* arg1, char* arg2) {
 	ds1->changeRHSBinding(rhs1);
 
 	ValPos* rhs2 = new StringValPos(11);
-	char* RHSVal2 = "Cstore00010";
+	char* RHSVal2 = "Cstore00049";
 	rhs2->set((byte*)RHSVal2);
-	//ds2->changeRHSBinding(rhs2);
+	ds2->changeRHSBinding(rhs2);
 	//Count* agg = new Count((Operator*) ds1, 0, (Operator*) ds1, 0);
 
 
